ffccbf149662d847344b4b56e21bb3b2c4a40854
arikakira <samanthal242@nycstudents.net>
2025-05-23 15:48:22 +0000

    Update

--- BlindMaze.java
+++ BlindMaze.java
@@ -1,1 +1,69 @@
+public class BlindMaze {
+    private int currentRow;
+    private int currentCol;
+    private String wall = "XXX";
+    private String emptySpace = "   ";
+    private String wheel = " 7 ";
+    private String start = " 0 ";
+    private String end = " W ";
+    //private String eye = "";
 
+    private String[][] maze = 
+    {
+        {"XXX","XXX","XXX","XXX","XXX","XXX","XXX","XXX","XXX","XXX","XXX","XXX","XXX","XXX","XXX"},
+        {"XXX","   ","   ","   ","XXX","   ","   ","   ","   ","   ","   ","XXX","   ","   ","XXX"},
+        {"XXX","   ","XXX","   ","XXX","   ","XXX","XXX","   ","XXX","XXX","XXX","   ","XXX","XXX"},
+        {"XXX","XXX","XXX","   ","   ","   ","   ","XXX","   ","   ","   ","XXX","   ","   ","XXX"},
+        {"XXX","   ","XXX","   ","XXX","XXX","   ","XXX","XXX","   ","   ","   ","   ","   ","XXX"},
+        {"XXX","   ","   ","   ","XXX","   ","   ","   ","   ","   ","XXX","   ","XXX","   ","XXX"},
+        {"XXX","   ","XXX","XXX","XXX","XXX","XXX","XXX","   ","XXX","XXX","   ","XXX","XXX","XXX"},
+        {"XXX","   ","XXX","   ","   ","XXX","   ","   ","   ","XXX","   ","   ","XXX","   ","XXX"},
+        {"XXX","   ","   ","   ","   ","XXX","XXX","   ","   ","   ","   ","   ","XXX","   ","XXX"},
+        {"XXX","   ","   ","XXX","   ","XXX","   ","   ","XXX","   ","XXX","   ","   ","   ","XXX"},
+        {"XXX","   ","XXX","XXX","   ","   ","   ","   ","XXX","XXX","XXX","XXX","   ","XXX","XXX"},
+        {"XXX","   ","   ","XXX","   ","XXX","XXX","   ","XXX","   ","XXX","   ","   ","   ","XXX"},
+        {"XXX","XXX","   ","XXX","   ","   ","XXX","   ","XXX","   ","   ","   ","XXX","XXX","XXX"},
+        {"XXX","   ","   ","XXX","XXX","^v^","XXX","   ","XXX","   ","XXX","   ","   ","   ","XXX"},
+        {"XXX","XXX","XXX","XXX","XXX","XXX","XXX","XXX","XXX","XXX","XXX","XXX","XXX","XXX","XXX"}
+    };
+
+    public BlindMaze(int r, int c) {
+        maze[r][c] = "0-0";
+        currentRow = r;
+        currentCol = c;
+    }
+
+    public void getMaze() {
+        for(int r=0; r<maze.length; r++) {
+            for(int c=0; c<maze[r].length; c++) {
+                System.out.print(maze[r][c]);
+            }
+            System.out.println();
+        }
+    }
+
+    public boolean rightAvail() {
+        return maze[currentRow][currentCol+1] == "   ";
+    }
+
+    public boolean leftAvail() {
+        return maze[currentRow][currentCol-1] == "   ";
+    }
+
+    public boolean upAvail() {
+        return maze[currentRow-1][currentCol] == "   ";
+    }
+
+    public boolean downAvail() {
+        return maze[currentRow+1][currentCol] == "   ";
+    }
+
+    public void move(String input) {
+        if(input == "right") {
+            if(rightAvail()) {
+                maze[currentRow][currentCol] = "   ";
+                maze[currentRow][currentCol+1] = "0-0";
+            }
+        }
+    }
+}
---
+++ Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        BlindMaze game = new BlindMaze(1, 6);
+        game.getMaze();
+
+        System.out.println(game.rightAvail());
+        System.out.println(game.leftAvail());
+        System.out.println(game.upAvail());
+        System.out.println(game.downAvail());
+        
+        System.out.print("The directions you can move in are:  ");
+        if(game.rightAvail()) System.out.print("right  ");
+        if(game.leftAvail()) System.out.print("left  ");
+        if(game.upAvail()) System.out.print("up  ");
+        if(game.downAvail()) System.out.print("down  ");
+        System.out.print("\n" + "Enter a direction: ");
+        String userInput = scn.nextLine();
+        System.out.println(userInput);
+        game.move(userInput);
+        game.getMaze();
+    }
+}
