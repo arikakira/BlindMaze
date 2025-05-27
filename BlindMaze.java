0b18d201f8bf0c3218444da4b18d900f7247ebee
arikakira <samanthal242@nycstudents.net>
2025-05-27 15:40:18 +0000

    Fully movable maze that ends when reachimng end

--- BlindMaze.java
+++ BlindMaze.java
@@ -6,6 +6,8 @@ public class BlindMaze {
     private String wheel = " 7 ";
     private String start = " 0 ";
     private String end = " W ";
+    private int endRow = 13;
+    private int endCol = 5;
     //private String eye = "";
 
     private String[][] maze = 
@@ -43,27 +45,70 @@ public class BlindMaze {
     }
 
     public boolean rightAvail() {
-        return maze[currentRow][currentCol+1] == "   ";
+        return maze[currentRow][currentCol+1] == "   " || maze[currentRow][currentCol+1] == "^v^";
     }
 
     public boolean leftAvail() {
-        return maze[currentRow][currentCol-1] == "   ";
+        return maze[currentRow][currentCol-1] == "   " || maze[currentRow][currentCol-1] == "^v^";
     }
 
     public boolean upAvail() {
-        return maze[currentRow-1][currentCol] == "   ";
+        return maze[currentRow-1][currentCol] == "   " || maze[currentRow-1][currentCol] == "^v^";
     }
 
     public boolean downAvail() {
-        return maze[currentRow+1][currentCol] == "   ";
+        return maze[currentRow+1][currentCol] == "   " || maze[currentRow+1][currentCol] == "^v^";
     }
 
-    public void move(String input) {
+    public boolean reachEnd() {
+        return (currentRow==endRow && currentCol == endCol);
+    }
+
+    public boolean move(String input) {
         if(input.equals("right")) {
             if(rightAvail()) {
                 maze[currentRow][currentCol] = "   ";
                 maze[currentRow][currentCol+1] = "0-0";
+                currentCol = currentCol + 1;
+                return true;
+            } else {
+                System.out.println("You can't move in that direction!");
+                return false;
             }
         }
+        if(input.equals("left")) {
+            if(leftAvail()) {
+                maze[currentRow][currentCol] = "   ";
+                maze[currentRow][currentCol-1] = "0-0";
+                currentCol = currentCol - 1;
+                return true;
+            } else {
+                System.out.println("You can't move in that direction!");
+                return false;
+            }
+        }
+        if(input.equals("up")) {
+            if(upAvail()) {
+                maze[currentRow][currentCol] = "   ";
+                maze[currentRow-1][currentCol] = "0-0";
+                currentRow = currentRow - 1;
+                return true;
+            } else {
+                System.out.println("You can't move in that direction!");
+                return false;
+            }
+        }
+        if(input.equals("down")) {
+            if(downAvail()) {
+                maze[currentRow][currentCol] = "   ";
+                maze[currentRow+1][currentCol] = "0-0";
+                currentRow = currentRow + 1;
+                return true;
+            } else {
+                System.out.println("You can't move in that direction!");
+                return false;
+            }
+        }
+        return false;
     }
 }
--- Main.java
+++ Main.java
@@ -3,25 +3,32 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
+        boolean sucessfulMove = false;
 
         BlindMaze game = new BlindMaze(1, 6);
-        game.getMaze();
 
-        System.out.println(game.rightAvail());
-        System.out.println(game.leftAvail());
-        System.out.println(game.upAvail());
-        System.out.println(game.downAvail());
+        while(!game.reachEnd()) {
+            game.getMaze();
+
+            System.out.println(game.rightAvail());
+            System.out.println(game.leftAvail());
+            System.out.println(game.upAvail());
+            System.out.println(game.downAvail());
         
-        System.out.print("The directions you can move in are:  ");
-        if(game.rightAvail()) System.out.print("right  ");
-        if(game.leftAvail()) System.out.print("left  ");
-        if(game.upAvail()) System.out.print("up  ");
-        if(game.downAvail()) System.out.print("down  ");
+            System.out.print("The directions you can move in are:  ");
+            if(game.rightAvail()) System.out.print("right  ");
+            if(game.leftAvail()) System.out.print("left  ");
+            if(game.upAvail()) System.out.print("up  ");
+            if(game.downAvail()) System.out.print("down  ");
         
-        System.out.print("\n" + "Enter a direction: ");
-        String userInput = scn.nextLine();
-        System.out.println(userInput);
-        game.move(userInput);
-        game.getMaze();
+            while(!sucessfulMove) {
+                System.out.print("\n" + "Enter a direction: ");
+                String userInput = scn.nextLine();
+                System.out.println(userInput);
+                sucessfulMove = game.move(userInput);
+            }
+            sucessfulMove = false;
+            game.getMaze();
+        }
     }
 }
